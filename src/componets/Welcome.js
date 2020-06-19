00084fa945085cd6a5ff6789bb14f71e369ed272
zjacobsdev <z.jacobsdev@gmail.com>
2020-06-18 21:43:18 -0400

    -added page sections

---
+++ src/componets/Welcome.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+function Welcome() {
+
+  const style ={
+    
+    
+    color: 'red',
+
+  }
+
+  return (
+    <div class ="sec">
+      <h1 style ={style}>Welcome to my awesome page </h1>
+    </div>
+
+  )
+
+    
+}
+
+export default Welcome;
--- src/componets/services.js
+++ src/componets/services.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 
 function Services() {
 
 
   return (
 
-    <div>Hi, Im Zahmir</div>
+    <div class ="sec">
+
+      <h1>Services </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque. Auctor augue mauris augue neque gravida in fermentum. Volutpat commodo sed egestas egestas fringilla. Consectetur libero id faucibus nisl. Volutpat consequat mauris nunc congue nisi vitae suscipit. Amet volutpat consequat mauris nunc. In nibh mauris cursus mattis molestie. Integer enim neque volutpat ac tincidunt vitae semper quis. Egestas egestas fringilla phasellus faucibus.</p>
+      
+    </div>
   )
     
 }
